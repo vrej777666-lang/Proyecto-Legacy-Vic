fd814f0099b2ebd1a1182849090aecc47d510f54
vrej777666-lang <vrej777666@gmail.com>
2026-02-19 18:51:58 -0600

    CapaPresentacionAdmin

---
+++ ADMIN_Controllers/HomeController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using CapaEntidad;
+using CapaNegocio;
+using ClosedXML.Excel;
+
+namespace CapaPresentacionAdmin.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Usuarios()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListarUsuarios()
+        {
+            List<Usuario> oLista = new List<Usuario>();
+            oLista = new CN_Usuarios().Listar();
+            // Usando LINQ oLista = new CN_Usuarios().Listar().Where(u => u.Restablecer == false).ToList();
+            // sino pones TOLIST al final da error, SUPONGO q es porque no RETORNARIA 1 LISTA ... averiguar
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public JsonResult GuardarUsuario(Usuario objeto)
+        {
+            Object resultado;
+            string mensaje = string.Empty;
+
+            if (objeto.IdUsuario == 0)
+            {
+                resultado = new CN_Usuarios().Registrar(objeto, out mensaje);
+            }
+            else
+            {
+                resultado = new CN_Usuarios().Editar(objeto, out mensaje);
+            }
+
+            return Json(new { resultado = resultado, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult EliminarUsuario(int id)
+        {
+            bool respuesta = false;
+            string mensaje = string.Empty;
+
+            respuesta = new CN_Usuarios().Eliminar(id, out mensaje);
+
+            return Json(new { resultado = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult VistaDashBoard()
+        {
+            DashBoard objeto = new CN_Reporte().VerDashBoard();
+
+            return Json(new { resultado = objeto }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult ListaReporteVentas(string fechainicio, string fechafin, string idtransaccion)
+        {
+            List<Reporte> oLista = new CN_Reporte().ReporteVentas(fechainicio, fechafin, idtransaccion);
+
+
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public FileResult ExportarReporteVentas(string fechainicio, string fechafin, string idtransaccion)
+        {
+            List<Reporte> olista = new List<Reporte>();
+            olista = new CN_Reporte().ReporteVentas(fechainicio, fechafin, idtransaccion);
+
+            DataTable dt = new DataTable();
+            dt.Locale = new System.Globalization.CultureInfo("es-GT");
+            dt.Columns.Add("Fecha Venta", typeof(string));
+            dt.Columns.Add("Cliente", typeof(string));
+            dt.Columns.Add("Producto", typeof(string));
+            dt.Columns.Add("Precio", typeof(decimal));
+            dt.Columns.Add("Cantidad", typeof(int));
+            dt.Columns.Add("Total", typeof(decimal));
+            dt.Columns.Add("IdTransaccion", typeof(string));
+
+            foreach (Reporte rp in olista)
+            {
+                dt.Rows.Add(new object[]
+                {
+                    rp.FechaVenta,
+                    rp.Cliente,
+                    rp.Producto,
+                    rp.Precio,
+                    rp.Cantidad,
+                    rp.Total,
+                    rp.IdTransaccion
+                });
+            }
+
+            dt.TableName = "Datos";
+
+            using (XLWorkbook wb = new XLWorkbook() ) {
+
+                wb.Worksheets.Add(dt);
+                using( MemoryStream stream = new MemoryStream())
+                {
+                    wb.SaveAs(stream);
+                    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "ReporteVenta" + DateTime.Now.ToString() + ".xlsx");
+                }
+
+            }       
+        }
+
+
+
+        [HttpGet]
+        public JsonResult ReporteVentas()
+        {
+            List<DashVentas> oLista = new CN_DashVenta().ObtenerVentas();
+            return Json(oLista, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpGet]
+        public JsonResult ReporteProductos()
+        {
+            List<DashProductos> oLista = new CN_DashProducto().ObtenerProductos();
+            return Json(oLista, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
